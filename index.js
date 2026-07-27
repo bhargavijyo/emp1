ca77a523eea5ee2bfd5bb98d77ff0501a3e9b8d6
bhargavijyo <bhargavasuthi@gmail.com>
2026-07-27 20:03:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var name = "RAM";
+var name = "Ravi";
+console.log(name);//Ravi 
+
+let age = 20;
+age=67;
+console.log(age);//67
+
+const p1 = 3.14;
+console.log(p1);//3.14
+
+console.log("Number");
+
+console.error("This is not correct");
+
+console.warn("Invalid msg");
+
+//console.clear();
+
+let a = alert("Welcome to Web dev");
+console.log(a);
+
+let b= confirm("Do you really want to learn new technologies?");
+console.log(b);
+
+let c = prompt("Enter your name:");
+console.log(c);
+
+document.writeln("Welcome to Full stack developer role");
+
+let e = 27;
+console.log(e);
+
+let f = "Bhuvan";
+console.log(f);
+
+let istrue = false;
+console.log(istrue);
+
+let ex;
+console.log(ex);
+
+let em = null;
+console.log(em);
